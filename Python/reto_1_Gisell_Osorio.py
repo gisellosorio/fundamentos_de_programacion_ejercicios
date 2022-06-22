f1d05815a384dcabef366adc4fb18880e1745110
gisellosorio <62785503+gisellosorio@users.noreply.github.com>
2022-06-22 15:05:35 -0700

    Update reto_1_Gisell_Osorio.py
    Organize comments

--- Python/reto_1_Gisell_Osorio.py
+++ Python/reto_1_Gisell_Osorio.py
@@ -1,4 +1,3 @@
-'''
 def CDT(usuario,capital,tiempo):
     if tiempo<=2:
         porcentaje_a_perder=0.02
@@ -8,23 +7,7 @@ def CDT(usuario,capital,tiempo):
         porcentaje_interes=0.03
         valor_intereses=capital*porcentaje_interes*tiempo/12
         valor_total=capital+valor_intereses
-    mensaje='Para el usuario '+str(usuario)+' La cantidad de dinero a recibir, según el monto inicial '+str(capital)+' para un tiempo de '+str(tiempo)+' meses es: '+str(valor_total)
-    return mensaje
-
-print(CDT('AB1012',1000000,3))
-print('\n')
-print(CDT('ER3366',650000,2))
-'''
-
-def CDT(usuario,capital,tiempo):
-    if tiempo<=2:
-        porcentaje_a_perder=0.02
-        valor_a_perder=capital*porcentaje_a_perder
-        valor_total=capital-valor_a_perder
-    else:
-        porcentaje_interes=0.03
-        valor_intereses=capital*porcentaje_interes*tiempo/12
-        valor_total=capital+valor_intereses
+    #mensaje='Para el usuario '+str(usuario)+' La cantidad de dinero a recibir, según el monto inicial '+str(capital)+' para un tiempo de '+str(tiempo)+' meses es: '+str(valor_total)
     mensaje=f'Para el usuario {usuario} La cantidad de dinero a recibir, según el monto inicial {capital} para un tiempo de {tiempo} meses es: {valor_total}'
     return mensaje
 
